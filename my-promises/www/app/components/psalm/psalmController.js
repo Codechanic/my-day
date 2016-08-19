f596fa545f7fd53d072f5409c25bddf286c1b7b5
felix <firomerorom4@gmail.com>
2016-08-19 16:00:21 -0400

    my-day-app: Corrigiendo navegacion

--- my-promises/www/app/components/psalm/psalmController.js
+++ my-promises/www/app/components/psalm/psalmController.js
@@ -3,9 +3,11 @@
  */
 angular.module('app.controllers').controller('PsalmController',PsalmController);
 
-function PsalmController($scope,$rootScope,PsalmsTree,Book) {
+function PsalmController($scope,$rootScope,PsalmsTree,Book,$timeout) {
     $scope.PsalmTree = PsalmsTree;
     $scope.category= {};
+    $scope.chapter= {};
+    $scope.psalm= -1;
     var Book = Book;
 
     $scope.toOpen = function (category) {
@@ -20,5 +22,30 @@ function PsalmController($scope,$rootScope,PsalmsTree,Book) {
 
     $scope.isOpen = function (category) {
         return category.title==$scope.category.title;
+    };
+    
+    $scope.showCategory=function () {
+        return $scope.psalm==-1;
+    };
+    
+    $scope.showPsalm=function () {
+      return   $scope.psalm!=-1;
+    };
+    
+    $scope.Reset=function () {
+
+        $timeout(function () {
+            $scope.$apply($scope.psaml=-1);
+            $scope.$apply($scope.chapter={});
+        },500);
+    };
+    $scope.openPsalm = function (psalm) {
+        if (psalm == $scope.psalm) {
+            $scope.psalm=-1;
+        }  
+        else{
+            $scope.psalm = parseInt(psalm);
+            $scope.chapter = Book.chapters[$scope.psalm-1];
+        }
     }
 }
